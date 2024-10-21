3f43e38b3ff2e803197e6ad3498ada771cb27da0
Cody-Talmadge <Cody.Talmadge@gmail.com>
2024-10-21 16:52:59 -0700

    Switched cluster load balancer to redis from python object (to allow synchronization across multiple workers)

--- cluster-load-balancer/cluster-load-balancer.py
+++ cluster-load-balancer/cluster-load-balancer.py
@@ -2,19 +2,20 @@ from flask import Flask, request, Response, jsonify
 import requests
 import random
 import time
+import redis
 
 app = Flask(__name__)
 
-server_load_status = {}
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 # Endpoint for load balancing requests
 @app.route('/')
 def load_balance():
-    delete_stale_servers()
 
     # Choose the target server randomly
-    if server_load_status:
-        target_ip = "http://" + random.choice(list(server_load_status.keys()))
+    server_ips = [ip.decode('utf-8') for ip in r.keys("user:*")]
+    if len(server_ips) > 0:
+        target_ip = "http://" + random.choice(server_ips)
     else:
         # If no servers are connected
         return Response("No connected servers", 503)
@@ -29,11 +30,12 @@ def receive_server_status():
     data = request.get_json()
     try:
         server_ip = data.get('server_ip')
-        server_load_status[server_ip] = {
+        r.hset(server_ip, mapping = {
             "average_cpu_usage": data.get("average_cpu_usage"),
             "average_request_duration": data.get("average_request_duration"),
             "last_updated": time.time()
-        }
+        })
+        r.expire(server_ip, 15)
         return Response(status=200)
     except:
         return Response("Invalid request", status=400)
@@ -41,18 +43,13 @@ def receive_server_status():
 # Endpoint for reporting server load status for all connected servers
 @app.route('/all_server_status', methods=['GET'])
 def report_all_server_status():
-    return jsonify(server_load_status)
-
-def delete_stale_servers():
-    current_time = time.time()
-    stale_servers = []
-    for server_ip, server_data in server_load_status.items():
-        # Remove the server from the list if it hasn't been updated in the last 15 seconds
-        if server_data['last_updated'] + 15 < current_time:
-            stale_servers.append(server_ip)
-    
-    for server_ip in stale_servers:
-        del server_load_status[server_ip]
+    server_info = []
+    server_ips = [ip.decode('utf-8') for ip in r.keys("Ass*")]
+    for ip in server_ips:
+        server_data = r.hgetall(ip)
+        decoded_data = {key.decode('utf-8'): value.decode('utf-8') for key, value in server_data.items()}
+        server_info.append(decoded_data)
+    return jsonify(server_info)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, threaded=True)
