ef15caf203dafc2933ba636552eb000854d8dfde
Cody-Talmadge <Cody.Talmadge@gmail.com>
2024-10-21 15:44:28 -0700

    Added setup script for cluster load balancer for generic ec2 instance

---
+++ cluster-load-balancer/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Download get-pip.py if it doesn't already exist
+if [ ! -f "get-pip.py" ]; then
+    echo "Downloading get-pip.py..."
+    curl -O https://bootstrap.pypa.io/get-pip.py
+else
+    echo "get-pip.py already exists. Skipping download."
+fi
+
+# Install pip if it doesn't exist
+if ! command -v pip &> /dev/null; then
+    echo "pip not found. Installing pip..."
+    sudo python3 get-pip.py
+else
+    echo "pip is already installed. Skipping pip installation."
+fi
+
+# Install required Python dependencies if they are not already installed
+REQUIRED_MODULES=("flask" "requests" "gunicorn")
+for MODULE in "${REQUIRED_MODULES[@]}"; do
+    if ! python3 -c "import ${MODULE}" &> /dev/null; then
+        echo "${MODULE} not found. Installing ${MODULE}..."
+        sudo -H pip install ${MODULE}
+    else
+        echo "${MODULE} is already installed. Skipping installation."
+    fi
+done
+
+# Verify installations
+echo "Verifying installations..."
+python3 -c "import flask, requests, gunicorn, random, time" && echo "All dependencies are successfully installed."
+
+# Create load-balancer.service for gunicorn
+cat <<EOL | sudo tee /etc/systemd/system/load-balancer.service
+[Unit]
+Description=Gunicorn instance to serve the load balancer
+After=network.target
+
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ec2-user/cluster-load-balancer
+ExecStart=/usr/local/bin/gunicorn --workers 4 --threads 10 --bind 0.0.0.0:80 cluster-load-balancer:app
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Reload systemd, enable and start load-balancer service
+sudo systemctl daemon-reload
+sudo systemctl enable load-balancer.service
+sudo systemctl start load-balancer.service
