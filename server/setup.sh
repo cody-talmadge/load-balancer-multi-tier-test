e3efd25a712cb77ca91923629b15974362c834e4
Cody-Talmadge <Cody.Talmadge@gmail.com>
2024-10-21 18:11:35 -0700

    Added psutil to modules to pull

--- server/setup.sh
+++ server/setup.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # Install required Python dependencies if they are not already installed
-REQUIRED_MODULES=("flask" "requests" "gunicorn" "redis")
+REQUIRED_MODULES=("flask" "requests" "gunicorn" "redis" "psutil")
 for MODULE in "${REQUIRED_MODULES[@]}"; do
     if ! python3 -c "import ${MODULE}" &> /dev/null; then
         echo "${MODULE} not found. Installing ${MODULE}..."
@@ -29,7 +29,7 @@ done
 
 # Verify installations
 echo "Verifying installations..."
-python3 -c "import flask, requests, gunicorn, random, time" && echo "All dependencies are successfully installed."
+python3 -c "import flask, requests, gunicorn, random, time, psutil" && echo "All dependencies are successfully installed."
 
 # Create services
 sudo cp flask-server.service /etc/systemd/system/flask-server.service
