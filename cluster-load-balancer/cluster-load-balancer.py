6d33901f4a5ab0921baecb7c4444715e9a8f60c0
Cody-Talmadge <Cody.Talmadge@gmail.com>
2024-11-14 16:07:53 -0800

    Fixed redis key expiration race condition

--- cluster-load-balancer/cluster-load-balancer.py
+++ cluster-load-balancer/cluster-load-balancer.py
@@ -13,7 +13,7 @@ r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 def get_internal_ip():
     try:
-        # Create a socket connection to Google's DNS server. We're not actually
+        # Create a socket connection to Google's DNS server We're not actually
         # sending any data, just using it to get the local IP of the server
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect("8.8.8.8")
@@ -34,7 +34,8 @@ def load_balance():
 
     # Choose the target server randomly - this logic will be changed to
     # the actual load balancer log
-    server_ips = [ip for ip in r.keys("*")]
+    server_status_keys = r.keys("server_status:*")
+    server_ips = [key.split(":")[1] for key in server_status_keys]
     if len(server_ips) > 0:
         target_ip = pick_server(server_ips)
         target_url = "http://" + target_ip
@@ -42,17 +43,17 @@ def load_balance():
         return Response("No connected servers", 503)
 
     if target_ip != overload_server:
-        r.hincrby(target_ip, 'active_requests')
-        r.hincrby(target_ip, 'req_curr_5')
+        r.hincrby(f"server_counts:{target_ip}", 'active_requests')
+        r.hincrby(f"server_counts:{target_ip}", 'req_curr_5')
     try:
         print("Target: " + target_url)
         resp = requests.get(target_url)
         if target_ip != overload_server:
-            r.hincrby(target_ip, 'active_requests', -1)
+            r.hincrby(f"server_counts:{target_ip}", 'active_requests', -1)
         return Response(resp.content, status=resp.status_code, headers=dict(resp.headers))
     except:
         if target_ip != overload_server:
-            r.hincrby(target_ip, 'active_requests', -1)
+            r.hincrby(f"server_counts:{target_ip}", 'active_requests', -1)
         return Response("Server error", status=503)
     
 
@@ -62,17 +63,18 @@ def receive_server_status():
     data = request.get_json()
     try:
         server_name = data.get('server_ip')
-        r.hset(server_name, 'cpu_usage', data.get('average_cpu_usage'))
-        r.hset(server_name, 'last_updated', time.time())
+        status_key = f"server_status:{server_ip}"
+        r.hset(status_key, 'cpu_usage', data.get('average_cpu_usage'))
+        r.hset(status_key, 'last_updated', time.time())
         req_last_5 = r.hget(server_name, 'req_curr_5')
         if req_last_5 is None:
             req_last_5 = 0
-        r.hset(server_name, 'req_curr_5' , 0)
-        r.hset(server_name, 'req_last_5', req_last_5)
+        r.hset(f"server_counts:{server_ip}", 'req_curr_5' , 0)
+        r.hset(f"server_counts:{server_ip}", 'req_last_5', req_last_5)
 
         # Expire servers after 15 seconds of not seeing them (they should report
         # status every 5 seconds)
-        r.expire(server_name, 15)
+        r.expire(status_key, 15)
         return Response(status=200)
     except:
         return Response("Request error", status=400)
@@ -81,11 +83,14 @@ def receive_server_status():
 @app.route('/all_server_status', methods=['GET'])
 def report_all_server_status():
     server_info = []
-    server_names = [name for name in r.keys("*")]
-    for name in server_names:
-        server_data = r.hgetall(name)
-        decoded_data = {key: value for key, value in server_data.items()}
-        server_info.append({name: decoded_data})
+    server_status_keys = r.keys("server_status:*")
+    for status_key in server_status_keys:
+        server_ip = status_key.split(":")[1]
+        status_data = r.hgetall(status_key)
+        counts_data = r.hgetall(f"server_counts:{server_ip}")
+        
+        decoded_data = {**status_data, **counts_data}
+        server_info.append({server_ip: decoded_data})
     return jsonify(server_info)
 
 def pick_server(server_ips):
@@ -109,7 +114,7 @@ def pick_server(server_ips):
         return lower_server
 
 def get_active_requests(server_ip):
-    active_requests = r.hget(server_ip, 'active_requests')
+    active_requests = r.hget(f"server_counts:{server_ip}", 'active_requests')
     if active_requests is None:
         return 0
     else:
